77bf52227eba403d93b129120d1bacf019755bff
Adriel3456 <the.dead.spy.squad.is.taken@gmail.com>
2025-11-11 20:01:37 -0700

    Update Bullet.py

--- Bullet.py
+++ Bullet.py
@@ -1,8 +1,30 @@
 import pygame
-pygame.init()
-screen=pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Hello")
+import os 
+pygame.font.init()
+pygame.mixer.init()
 
-screen.fill((123, 223, 25))
-pygame.draw.circle(screen, (255, 0, 0), (250,250), 50)
-pygame.display.update()
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game!")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+
+#Bullet_HIT_SOUND = pygame.mixer.sound('assets/granade+1.mp3)
+#Bullet_FIRE_sound = pygame.mixer.sound('assets/guns+silencer.mp3)
+
+HEALTH_FONT = pygame.sysfont('comicsans', 40)
+WINNER_FONT = pygame.sysfont('comicsans', 100)
+
+FPS = 60
+VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
